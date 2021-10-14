aee5d2b6ce64c9e624ed3bda14047de507ba8aba
JuanFeBlancoT <juanfeblancot@gmail.com>
2021-10-13 23:16:20 -0500

    Initial commit

---
+++ src/view/MainServer.java
@@ -0,0 +1,22 @@
+package view;
+
+import processing.core.PApplet;
+
+public class MainServer extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main("view.MainServer");
+	}
+
+	public void settings() {
+		size(1000,200);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		background(80);
+	}
+}
